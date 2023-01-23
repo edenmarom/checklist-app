610fc32006c93bb39bbdfa52c9618d1f6a55e8ae
Eden Marom <EdenMarom@gmail.com>
2023-01-23 22:05:15 +0200

    upload profile pic from gallery

--- app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.java
@@ -1,9 +1,13 @@
 package com.example.myapplication.ui.profile;
 
 import static com.example.myapplication.ui.data.LoginRepository.currentUser;
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,7 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -20,11 +25,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class ProfileFragment extends Fragment {
 
@@ -32,6 +41,7 @@ public class ProfileFragment extends Fragment {
     FirebaseStorage storage = FirebaseStorage.getInstance();
     private final String userImagesDBLocation = "user-images/%s.png";
     private final String dbUrl = "https://checklist-f8ac0-default-rtdb.europe-west1.firebasedatabase.app";
+    StorageReference storageRef = storage.getReference();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,17 +49,21 @@ public class ProfileFragment extends Fragment {
                 new ViewModelProvider(this).get(ProfileViewModel.class);
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        FloatingActionButton editImgBtn = binding.profileEditBtn;
+        editImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
         LoadProfileImage();
         LoadProfileData();
-
         return root;
     }
 
     private void LoadProfileImage() {
         if (currentUser!= null){
             ImageView imageIV = binding.profileImage;
-            StorageReference storageRef = storage.getReference();
             String userUID = currentUser.getUserId();
             String path = String.format(userImagesDBLocation, userUID);
             StorageReference imageRef = storageRef.child(path);
@@ -58,11 +72,7 @@ public class ProfileFragment extends Fragment {
                         @Override
                         public void onSuccess(byte[] bytes) {
                             Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                            DisplayMetrics dm = new DisplayMetrics();
-                            getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-                            imageIV.setMinimumHeight(dm.heightPixels);
-                            imageIV.setMinimumWidth(dm.widthPixels);
-                            imageIV.setImageBitmap(bm);
+                            bitmapToImg(bm, imageIV);
                             Log.d("TAG", "load profile:success");
                         }
                     }).addOnFailureListener(new OnFailureListener() {
@@ -73,6 +83,14 @@ public class ProfileFragment extends Fragment {
                         }
                     });
         }
+    }
+
+    private void bitmapToImg(Bitmap bm, ImageView imageIV) {
+        DisplayMetrics dm = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+        imageIV.setMinimumHeight(dm.heightPixels);
+        imageIV.setMinimumWidth(dm.widthPixels);
+        imageIV.setImageBitmap(bm);
     }
 
     private void LoadProfileData() {
@@ -95,6 +113,64 @@ public class ProfileFragment extends Fragment {
                         phoneTV.setText(task.getResult().child("phone").getValue().toString());
                         emailTV.setText(task.getResult().child("email").getValue().toString());
                     }
+                }
+            });
+        }
+    }
+
+    void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        ChooseNewProfileImage.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> ChooseNewProfileImage
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap;
+                        try {
+                            selectedImageBitmap = MediaStore.Images.Media.getBitmap(
+                                    requireActivity().getContentResolver(),
+                                    selectedImageUri);
+                            UploadSelectedImg(selectedImageBitmap);
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+    private void UploadSelectedImg(Bitmap selectedImageBitmap) {
+        if (currentUser!= null) {
+            String userUID = currentUser.getUserId();
+            String path = String.format(userImagesDBLocation, userUID);
+            StorageReference imageRef = storageRef.child(path);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            selectedImageBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] imgData = baos.toByteArray();
+            UploadTask uploadTask = imageRef.putBytes(imgData);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Log.d("TAG", "load profile:fail");
+                    Log.d("TAG", "path: " + path);
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Log.d("TAG", "edit profile:success");
+                    ImageView profileImageIV = binding.profileImage;
+                    bitmapToImg(selectedImageBitmap, profileImageIV);
                 }
             });
         }
