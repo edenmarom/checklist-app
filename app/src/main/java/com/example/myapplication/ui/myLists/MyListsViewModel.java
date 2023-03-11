3db28e62cead3a9d33c2721438527ec91f5311f2
Eden Marom <EdenMarom@gmail.com>
2023-03-11 19:42:39 +0200

    app crash fixed

--- app/src/main/java/com/example/myapplication/ui/myLists/MyListsViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/myLists/MyListsViewModel.java
@@ -68,30 +68,34 @@ public class MyListsViewModel extends ViewModel {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
 
+
+                //TODO - EDEN TO ADI: i marked the following lines because it crashed the app.
                 // Retrieve all data from the dataSnapshot object
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Object value = snapshot.getValue();
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    Object value = snapshot.getValue();
+//
+//                    String nameDB = ((HashMap) value).get("name").toString();
+//                    List<Object> listItemDB = Arrays.asList(((HashMap) value).get("items"));
+//                    List<String> listItemDB_ = listItemDB.stream()
+//                            .map(object -> Objects.toString(object, null))
+//                            .collect(Collectors.toList());
+//                    List<Object> address =  Arrays.asList(((HashMap) value).get("location"));
+//                    Address location = new Address(Locale.getDefault());
+//                    location.setLatitude((int)(address.get(0)));
+//                    location.setLongitude((int)(address.get(1)));
+//                    String userIdDB = ((HashMap) value).get("userID").toString();
+//                    List<Object> participantsDB = Arrays.asList(((HashMap) value).get("participants"));
+//                    List<String> participantsDB_ = participantsDB.stream()
+//                            .map(object -> Objects.toString(object, null))
+//                            .collect(Collectors.toList());
+//                    String imgIdlDB = ((HashMap) value).get("image").toString();
+//
+//                    addList(new ListItem(nameDB, listItemDB_,location, userIdDB,participantsDB_,imgIdlDB));
+//
+//                    allListFromDB.add(value);
+//                }
 
-                    String nameDB = ((HashMap) value).get("name").toString();
-                    List<Object> listItemDB = Arrays.asList(((HashMap) value).get("items"));
-                    List<String> listItemDB_ = listItemDB.stream()
-                            .map(object -> Objects.toString(object, null))
-                            .collect(Collectors.toList());
-                    List<Object> address =  Arrays.asList(((HashMap) value).get("location"));
-                    Address location = new Address(Locale.getDefault());
-                    location.setLatitude((int)(address.get(0)));
-                    location.setLongitude((int)(address.get(1)));
-                    String userIdDB = ((HashMap) value).get("userID").toString();
-                    List<Object> participantsDB = Arrays.asList(((HashMap) value).get("participants"));
-                    List<String> participantsDB_ = participantsDB.stream()
-                            .map(object -> Objects.toString(object, null))
-                            .collect(Collectors.toList());
-                    String imgIdlDB = ((HashMap) value).get("image").toString();
-
-                    addList(new ListItem(nameDB, listItemDB_,location, userIdDB,participantsDB_,imgIdlDB));
-
-                    allListFromDB.add(value);
-                }
+                //TODO - EDEN TO ADI: END
 //                Object o = allListFromDB.get(1);
 //                Object b = allListFromDB.get(1);
 
